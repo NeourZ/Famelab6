6abefccaed23fd8ba3634afa8dc24d86804a17cc
Fame <pattanitfame@gmail.com>
2023-12-01 16:42:49 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,32 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int x = 1 ;
+    int i = 0 ;
+    int count = 0 ;
+    int number[100] ;
+    while (x != 0 && count < 100){
+        cout << "Enter an integer: " ;
+        cin >> x ;
+        if (x != 0) {
+            number[count] = x ;
+            count++ ;
+        }
+    }
+    int sumevens = 0 ;
+    int sumodds = 0 ;
+    while (i < count){
+        if (number[i] % 2 == 0 ){
+            sumevens ++ ;
+        }
+        else{
+            sumodds ++ ;
+        }
+        i ++ ;
+    }
+    cout << "#Even numbers = " ;
+    cout << sumevens << endl;
+    cout << "#Odd numbers = " ;
+    cout << sumodds << endl;
     return 0;
 }
